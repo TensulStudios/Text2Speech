c78a5dd0662f3d7744c6d1a7d3a9640a554b5f4f
Temporary <tunfulstudios@gmail.com>
2025-09-25 17:28:29 +0100

    Update plauder.js

--- api/plauder.js
+++ api/plauder.js
@@ -1,8 +1,4 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
   try {
     // Forward the request to fasthub
     const response = await fetch("https://fasthub.net/plauder", {
